3bc862cae3f4edba2ea4d62c5723d4a17707ff9e
Ahmad Rufai <tigressfx@gmail.com>
2023-08-22 18:47:54 -0700

    yy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _ returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
